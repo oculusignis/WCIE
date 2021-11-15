cb246c443f095820726856c7ce43fe6bd91cb388
Luca Bruegger <luca.bruegger@ost.ch>
2021-11-15 14:52:41 +0100

    Doxygen Implemented

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 
+##API URL
 url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
 r = requests.get(url)
 string = r.content.decode("utf-8")
