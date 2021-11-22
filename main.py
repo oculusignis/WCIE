5d8a694a72462611eb4c7237822f23fc0b284269
Luca Bruegger <luca.bruegger@ost.ch>
2021-11-22 15:47:31 +0100

    delete the brackets for the ReadFile Function

--- main.py
+++ main.py
@@ -7,4 +7,11 @@ def savetofile (dataDictionary):
     """save dictionary to file line per line (key and value separated by comma)"""
     with open('tempData.txt', 'w') as f:
         for key, value in dataDictionary.items():
-            f.write(str(key) + "," + str(value) + "\n")
+            newval = str(value)
+            newval = newval.replace("[", "")
+            newval = newval.replace("]", "")
+            f.write(str(key) + "," + newval + "\n")
+
+testdic = {"mine" : [1, 2, 3],
+           "test" : [5, 6]}
+savetofile(testdic)
