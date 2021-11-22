82f464db6b867004bbeb186fa71a81f57b302321
Luca Bruegger <luca.bruegger@ost.ch>
2021-11-22 14:39:41 +0100

    save List to File

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 import requests
 
-##API URL
-url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
-r = requests.get(url)
-string = r.content.decode("utf-8")
+def savetofile (dataList):
+    with open('tempData.txt', 'w') as f:
+        for item in dataList:
+            f.write("%s\n" % item)
 
-print(string)
-print(string[5])  # me like <- git test
+
+
+##API URL
+#url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
+#r = requests.get(url)
+#string = r.content.decode("utf-8")
+
+#print(string)
+#print(string[5])  # me like <- git test
+
+#savetofile(string)
+
+my_list = [1, 2, 3]
+savetofile(my_list)
