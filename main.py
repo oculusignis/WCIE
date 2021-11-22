65f2f3aa27495cd43904f4ec50a4508d466dc3a9
Gian-Luca Brazerol <gianluca.brazerol@ost.ch>
2021-11-22 15:49:09 +0100

    Implemented function fileScanner

--- main.py
+++ main.py
@@ -1,12 +1,29 @@
 import requests
 
+def fileScanner():
+    """Returns a dictionary with location name as key and a string of water temperatures as value.
+    Locations and values read in from text file "tempData.txt" """
+    dataDict = dict()
+    maxsplit = 1
+    with open("tempData.txt") as file:
+        for line in file:                                   #iteration line by line
+            dataString = line.rstrip()                      #reads line into string
+            dataList = dataString.split(",", maxsplit)      #splits string into 2 substrings after first comma
+            dataDict[dataList[0]] = dataList[1]             #adds element to dictionary: 1. substring as key, 2. substring as value
+    return dataDict
+
+print(fileScanner())
+
 ##API URL
-url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
+url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1637478000000/1637485200000/1"
 r = requests.get(url)
 string = r.content.decode("utf-8")
-
 print(string)
-print(string[5])  # me like <- git test
 
-print("Useless string")
+
+
+
+
+
+
 
