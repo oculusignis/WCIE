6772756b98b5519580a3f292b53b95fda672e569
Luca Bruegger <luca.bruegger@ost.ch>
2021-11-22 15:04:49 +0100

    save Dictionary to File

--- main.py
+++ main.py
@@ -1,21 +1,7 @@
 import requests
 
-def savetofile (dataList):
+def savetofile (dataDictionary):
     with open('tempData.txt', 'w') as f:
-        for item in dataList:
-            f.write("%s\n" % item)
+        for key, value in dataDictionary.items():
+            f.write(str(key) + "," + str(value) + "\n")
 
-
-
-##API URL
-#url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
-#r = requests.get(url)
-#string = r.content.decode("utf-8")
-
-#print(string)
-#print(string[5])  # me like <- git test
-
-#savetofile(string)
-
-my_list = [1, 2, 3]
-savetofile(my_list)
