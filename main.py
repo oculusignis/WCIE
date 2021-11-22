7535d65fa2b0c60501f4a57f183a99a284ec63a1
Marc Lippuner <lippuner.marc@ost.ch>
2021-11-22 15:49:50 +0100

    made function templist, for data conversion

--- main.py
+++ main.py
@@ -1,9 +1,19 @@
 import requests
 
+
+def templist(url):
+    """converts the url response to a list containing only the temperatures"""
+    # get the data and save as string
+    data_string = requests.get(url).content.decode("utf8")
+    # convert string to list and remove first element representing depth
+    data_list = data_string[data_string.find("\n")+1:].split(",")
+    data_list.pop(0)
+    # convert strings in list to float
+    float_list = [float(x) for x in data_list]
+    return float_list
+
+
 ##API URL
 url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
-r = requests.get(url)
-string = r.content.decode("utf-8")
-
-print(string)
-print(string[5])  # me like <- git test
+temp_list = templist(url)
+print(temp_list)
