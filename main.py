e0e08eb167519a1b7c0e8edf8ba118cb8d256024
Luca Bruegger <luca.bruegger@ost.ch>
2021-11-22 15:22:57 +0100

    Save Dictionary to txt-File

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import requests
 
+## Function to save a Dictionary
+# The Dictionary will be saved line per line 
+# with the key and the value separated by a comma.
 def savetofile (dataDictionary):
+    """save dictionary to file line per line (key and value separated by comma)"""
     with open('tempData.txt', 'w') as f:
         for key, value in dataDictionary.items():
             f.write(str(key) + "," + str(value) + "\n")
-
