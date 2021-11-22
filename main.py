d633b76cfc5832d891b1a4c6a74f052c35e741d0
Franco Caminada <franco.caminada@ost.ch>
2021-11-22 16:41:50 +0100

    prints removed

--- main.py
+++ main.py
@@ -5,32 +5,24 @@ def timestamp():
     import datetime as dt
     # get the current GTM time zone date and time
     current_date_gtm = dt.datetime.utcnow()
-    print(current_date_gtm)
 
     # get the current local time zone date and time (for future use)
     # current_date = dt.datetime.now()
-    # print(current_date)
 
     # convert current GTM date to string with current time at 00:00:00
     string_date_start = current_date_gtm.strftime("%Y-%m-%d 00:00:00")
-    print(string_date_start)
     # convert current GTM date to string with current time at 21:00:00
     string_date_end = current_date_gtm.strftime("%Y-%m-%d 21:00:00")
-    print(string_date_end)
 
     #convert day start string into object
     date_object_start = dt.datetime.strptime(string_date_start, "%Y-%m-%d %H:%M:%S")
-    print(date_object_start)
     # convert day end string into object
     date_object_end = dt.datetime.strptime(string_date_end, "%Y-%m-%d %H:%M:%S")
-    print(date_object_end)
 
     #Multiply the timestamp of the datetime object day start by 1'000 to convert into millisec and round to remove .0
     millisecstart = round(date_object_start.timestamp() * 1000)
-    print(millisecstart)
     # Multiply the timestamp of the datetime object day end by 1'000 to convert into millisec and round to remove .0
     millisecend = round(date_object_end.timestamp() * 1000)
-    print(millisecend)
 
     return millisecstart, millisecend
 
