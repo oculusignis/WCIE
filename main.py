69699bdfb1de1f91dc6dd0f429d157bb6626fb3e
Gian-Luca Brazerol <gianluca.brazerol@ost.ch>
2021-11-24 20:44:48 +0100

    implemented dictPrinter, tabulator yet to be implemented

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import requests
+import datetime
+from tabulate import tabulate
+
 
 def templist(url):
     """converts the url response to a list containing only the temperatures"""
@@ -10,20 +13,33 @@ def templist(url):
     # convert strings in list to float
     float_list = [float(x) for x in data_list]
     return float_list
-  
-def fileScanner():
+
+
+def file_scanner():
     """Returns a dictionary with location name as key and a string of water temperatures as value.
     Locations and values read in from text file "tempData.txt" """
-    dataDict = dict()
-    maxsplit = 1
+    data_dict = dict()
     with open("tempData.txt") as file:
-        for line in file:                                   #iteration line by line
-            dataString = line.rstrip()                      #reads line into string
-            dataList = dataString.split(",", maxsplit)      #splits string into 2 substrings after first comma
-            dataDict[dataList[0]] = dataList[1]             #adds element to dictionary: 1. substring as key, 2. substring as value
-    return dataDict
+        for line in file:                             # iteration line by line
+            data_string = line.rstrip()               # reads line into string
+            data_list = data_string.split(",")        # splits string into substrings after every comma
+            data_dict[data_list[0]] = list(float(x) for x in data_list[1:-1]) # adds element to dictionary:
+            # 1. substring as key, others as float numbers
+    return data_dict
+
+
+def dict_printer(data_dict):
+    """"Takes a dictionary and prints it to the console: Location: Value (actual time)"""
+    for key in data_dict:
+        print(f"{key}: {data_dict[key][int(datetime.datetime.now().hour/3)]}")
+
+
+dict_printer(file_scanner())
+
+
+
 
 ##API URL
 url = "http://meteolakes.ch/api/coordinates/534700/144950/geneva/temperature/1537034400000/1537768800000/20"
 temp_list = templist(url)
-print(temp_list)
+# print(temp_list)
